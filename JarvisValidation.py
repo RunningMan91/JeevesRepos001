cd4833ace661e642f812eb927446f90c60e720ab
RunningMan91 <93554915+RunningMan91@users.noreply.github.com>
2022-06-25 22:47:50 +1000

    Update JarvisValidation.py

--- JarvisValidation.py
+++ JarvisValidation.py
@@ -109,6 +109,11 @@ def isvalid_room_type(room_type):
     return room_type.lower() in room_types
 
 
+def isvalid_view_preference(view_preference):
+    view_preferences = ['view', 'no view', 'either']
+    return view_preference.lower() in view_preferences
+
+
 def isvalid_date(date):
     try:
         dateutil.parser.parse(date)
@@ -137,9 +142,86 @@ def build_validation_result(isvalid, violated_slot, message_content):
     }
 
 
-"""
-Begin from def validate_book_car
-"""
+def validate_book_car(slots):
+    pickup_city = try_ex(lambda: slots['PickUpCity'])
+    pickup_date = try_ex(lambda: slots['PickUpDate'])
+    return_date = try_ex(lambda: slots['ReturnDate'])
+    driver_age = safe_int(try_ex(lambda: slots['DriverAge']))
+    car_type = try_ex(lambda: slots['CarType'])
+
+    if pickup_city and not isvalid_city(pickup_city):
+        return build_validation_result(
+            False,
+            'PickUpCity',
+            'We currently do not support {} as a valid destination.  Can you try a different city?'.format(pickup_city)
+        )
+
+    if pickup_date:
+        if not isvalid_date(pickup_date):
+            return build_validation_result(False, 'PickUpDate', 'I did not understand your departure date.  When would you like to pick up your car rental?')
+        if datetime.datetime.strptime(pickup_date, '%Y-%m-%d').date() <= datetime.date.today():
+            return build_validation_result(False, 'PickUpDate', 'Reservations must be scheduled at least one day in advance.  Can you try a different date?')
+
+    if return_date:
+        if not isvalid_date(return_date):
+            return build_validation_result(False, 'ReturnDate', 'I did not understand your return date.  When would you like to return your car rental?')
+
+    if pickup_date and return_date:
+        if dateutil.parser.parse(pickup_date) >= dateutil.parser.parse(return_date):
+            return build_validation_result(False, 'ReturnDate', 'Your return date must be after your pick up date.  Can you try a different return date?')
+
+        if get_day_difference(pickup_date, return_date) > 30:
+            return build_validation_result(False, 'ReturnDate', 'You can reserve a car for up to thirty days.  Can you try a different return date?')
+
+    if driver_age is not None and driver_age < 18:
+        return build_validation_result(
+            False,
+            'DriverAge',
+            'Your driver must be at least eighteen to rent a car.  Can you provide the age of a different driver?'
+        )
+
+    if car_type and not isvalid_car_type(car_type):
+        return build_validation_result(
+            False,
+            'CarType',
+            'I did not recognize that model.  What type of car would you like to rent?  '
+            'Popular cars are economy, midsize, or luxury')
+
+    return {'isValid': True}
+
+
+def validate_hotel(slots):
+    location = try_ex(lambda: slots['AU_Location'])
+    checkin_date = try_ex(lambda: slots['CheckInDate'])
+    nights = safe_int(try_ex(lambda: slots['Nights']))
+    room_type = try_ex(lambda: slots['RoomType'])
+    view_preference = try_ex(lambda: slots['ViewPreference'])
+
+    if location and not isvalid_city(location):
+        return build_validation_result(
+            False,
+            'AU_Location',
+            'We currently do not support {} as a valid destination.  Can you try a different city?'.format(location)
+        )
+
+    if checkin_date:
+        if not isvalid_date(checkin_date):
+            return build_validation_result(False, 'CheckInDate', 'I did not understand your check in date.  When would you like to check in?')
+        if datetime.datetime.strptime(checkin_date, '%Y-%m-%d').date() <= datetime.date.today():
+            return build_validation_result(False, 'CheckInDate', 'Reservations must be scheduled at least one day in advance.  Can you try a different date?')
+
+    if nights is not None and (nights < 1 or nights > 30):
+        return build_validation_result(
+            False,
+            'Nights',
+            'You can make a reservations for from one to thirty nights.  How many nights would you like to stay for?'
+        )
+
+    if room_type and not isvalid_room_type(room_type):
+        return build_validation_result(False, 'RoomType', 'I did not recognize that room type.  Would you like to stay in a queen, king, or deluxe room?')
+
+    return {'isValid': True}
+
 
 
 # --- Main handler sample code to edit---
